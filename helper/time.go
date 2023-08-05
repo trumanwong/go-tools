673460b9bb2beebf03288a6b0f676a016cf87f1e
trumanwong <1415829005@qq.com>
2023-08-05 10:50:14 +0800

    modify

--- helper/time.go
+++ helper/time.go
@@ -10,37 +10,59 @@ func ParseTime(t string) (*time.Time, error) {
 	return &res, nil
 }
 
-// WeekIntervalTime 获取某周的开始和结束时间,week为0本周,-1上周，1下周以此类推
-func WeekIntervalTime(week int) (startTime, endTime time.Time) {
-	now := time.Now()
-	offset := int(time.Monday - now.Weekday())
-	//周日做特殊判断 因为time.Monday = 0
-	if offset > 0 {
-		offset = -6
+type IntervalTimeType int
+
+const (
+	Day IntervalTimeType = iota
+	Week
+	Month
+	Year
+)
+
+type GetIntervalTimeRequest struct {
+	// 指定日期
+	Time time.Time
+	Type IntervalTimeType
+	// 如果类型为Day， 则Num为0指定日期的当天，-1指定日期的前一天，1指定日期的后一天以此类推
+	// 如果类型为Week， 则Num为0指定日期的当周,-1指定日期的上周，1指定日期的下周以此类推
+	// 如果类型为Month， 则Num为0指定日期的当月,-1指定日期的上月，1指定日期的下月以此类推
+	// 如果类型为Year， 则Num为0指定日期的当年,-1指定日期的去年，1指定日期的明年以此类推
+	Num int
+}
+
+type GetIntervalTimeResponse struct {
+	StartAt time.Time
+	EndAt   time.Time
+}
+
+// GetIntervalTime 获取某天/周/月/年的开始和结束时间,d为0今天,-1昨天，1明天以此类推
+func GetIntervalTime(req *GetIntervalTimeRequest) (resp *GetIntervalTimeResponse) {
+	resp = new(GetIntervalTimeResponse)
+	var offset int
+	switch req.Type {
+	case Day:
+		resp.StartAt, _ = time.ParseInLocation(time.DateTime, req.Time.Format(time.DateOnly)+" 00:00:00", time.Local)
+		resp.EndAt, _ = time.ParseInLocation(time.DateTime, req.Time.Format(time.DateOnly)+" 23:59:59", time.Local)
+	case Week:
+		year, month, day := req.Time.Date()
+		offset = int(time.Monday - req.Time.Weekday())
+		//周日做特殊判断 因为time.Monday = 0
+		if offset > 0 {
+			offset = -6
+		}
+		thisWeek := time.Date(year, month, day, 0, 0, 0, 0, time.Local)
+		resp.StartAt, _ = time.ParseInLocation(time.DateTime, thisWeek.AddDate(0, 0, offset+7*req.Num).Format(time.DateOnly)+" 00:00:00", time.Local)
+		resp.EndAt, _ = time.ParseInLocation(time.DateTime, thisWeek.AddDate(0, 0, offset+6+7*req.Num).Format(time.DateOnly)+" 23:59:59", time.Local)
+	case Month:
+		year, month, _ := req.Time.AddDate(0, req.Num, 0).Date()
+		thisMonth := time.Date(year, month, 1, 0, 0, 0, 0, time.Local)
+		resp.StartAt, _ = time.ParseInLocation(time.DateTime, thisMonth.Format(time.DateOnly)+" 00:00:00", time.Local)
+		resp.EndAt, _ = time.ParseInLocation(time.DateTime, thisMonth.AddDate(0, 1, -1).Format(time.DateOnly)+" 23:59:59", time.Local)
+	case Year:
+		year, _, _ := req.Time.AddDate(req.Num, 0, 0).Date()
+		thisYear := time.Date(year, 1, 1, 0, 0, 0, 0, time.Local)
+		resp.StartAt, _ = time.ParseInLocation(time.DateTime, thisYear.Format(time.DateOnly)+" 00:00:00", time.Local)
+		resp.EndAt, _ = time.ParseInLocation(time.DateTime, thisYear.AddDate(1, 0, -1).Format(time.DateOnly)+" 23:59:59", time.Local)
 	}
-
-	year, month, day := now.Date()
-	thisWeek := time.Date(year, month, day, 0, 0, 0, 0, time.Local)
-	startTime, _ = time.ParseInLocation("2006-01-02 15:04:05", thisWeek.AddDate(0, 0, offset+7*week).Format("2006-01-02")+" 00:00:00", time.Local)
-	endTime, _ = time.ParseInLocation("2006-01-02 15:04:05", thisWeek.AddDate(0, 0, offset+6+7*week).Format("2006-01-02")+" 23:59:59", time.Local)
-
-	return
-}
-
-// MonthIntervalTime 获取某月的开始和结束时间,m为0本月,-1上月，1下月以此类推
-func MonthIntervalTime(m int) (startTime, endTime time.Time) {
-	year, month, _ := time.Now().AddDate(0, m, 0).Date()
-	thisMonth := time.Date(year, month, 1, 0, 0, 0, 0, time.Local)
-	startTime, _ = time.ParseInLocation("2006-01-02 15:04:05", thisMonth.Format("2006-01-02")+" 00:00:00", time.Local)
-	endTime, _ = time.ParseInLocation("2006-01-02 15:04:05", thisMonth.AddDate(0, 1, -1).Format("2006-01-02")+" 23:59:59", time.Local)
-	return
-}
-
-// YearIntervalTime 获取某年的开始和结束时间,y为0今年,-1去年，1明年以此类推
-func YearIntervalTime(y int) (startTime, endTime time.Time) {
-	year, _, _ := time.Now().AddDate(y, 0, 0).Date()
-	thisYear := time.Date(year, 1, 1, 0, 0, 0, 0, time.Local)
-	startTime, _ = time.ParseInLocation("2006-01-02 15:04:05", thisYear.Format("2006-01-02")+" 00:00:00", time.Local)
-	endTime, _ = time.ParseInLocation("2006-01-02 15:04:05", thisYear.AddDate(1, 0, -1).Format("2006-01-02")+" 23:59:59", time.Local)
 	return
 }
--- helper/time_test.go
+++ helper/time_test.go
@@ -3,10 +3,32 @@ package helper
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestParseTime(t *testing.T) {
 	parseTime, err := ParseTime("")
 	assert.NotNil(t, err)
 	assert.Nil(t, parseTime)
+}
+
+func TestGetIntervalTime(t *testing.T) {
+	req := &GetIntervalTimeRequest{
+		Type: Day,
+		Num:  0,
+	}
+	req.Time, _ = time.ParseInLocation(time.DateTime, "2023-08-05 00:00:00", time.Local)
+	// test day
+	startAt, _ := time.ParseInLocation(time.DateTime, "2023-08-05 00:00:00", time.Local)
+	endAt, _ := time.ParseInLocation(time.DateTime, "2023-08-05 23:59:59", time.Local)
+	resp := GetIntervalTime(req)
+	assert.Equal(t, startAt, resp.StartAt)
+	assert.Equal(t, endAt, resp.EndAt)
+	// test week
+	req.Type = Week
+	startAt, _ = time.ParseInLocation(time.DateTime, "2023-07-31 00:00:00", time.Local)
+	endAt, _ = time.ParseInLocation(time.DateTime, "2023-08-06 23:59:59", time.Local)
+	resp = GetIntervalTime(req)
+	assert.Equal(t, startAt, resp.StartAt)
+	assert.Equal(t, endAt, resp.EndAt)
 }
