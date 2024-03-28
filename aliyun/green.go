e6add5fab1dfe9921879ad7bb72b3e302006bcbe
trumanwong <1415829005@qq.com>
2024-03-28 14:05:44 +0800

    add ImageModerationAdvance

--- aliyun/green.go
+++ aliyun/green.go
@@ -115,3 +115,62 @@ func (c GreenClient) ImageModeration(req *ImageModerationRequest) (info *ImageMo
 	}
 	return info, errors.New("image moderation failed. statusCode:" + tea.ToString(statusCode))
 }
+
+func (c GreenClient) ImageModerationAdvance(req *ImageModerationRequest) (list []*ImageModerationInfo, err error) {
+	// Runtime parameter settings, only effective for requests using this runtime parameter instance
+	runtime := &util.RuntimeOptions{}
+
+	// Build the image detection request.
+	m := map[string]interface{}{
+		// The ID of the data to be checked.
+		"dataId": uuid.New(),
+	}
+	if req.ImageUrl != nil {
+		m["imageUrl"] = *req.ImageUrl
+	} else if req.OssBucketName != nil && req.OssObjectName != nil && req.OssRegionID != nil {
+		// Use OSS authorization for detection
+		m["ossBucketName"] = *req.OssBucketName
+		m["ossObjectName"] = *req.OssObjectName
+		m["ossRegionId"] = *req.OssRegionID
+	}
+	serviceParameters, _ := json.Marshal(m)
+	service := tea.String("baselineCheck")
+	if req.Service != nil {
+		service = req.Service
+	}
+	imageModerationRequest := &green20220302.ImageModerationRequest{
+		// Image detection service: the serviceCode configured by the content security console image enhanced version rules, example: baselineCheck
+		Service:           service,
+		ServiceParameters: tea.String(string(serviceParameters)),
+	}
+
+	response, err := c.client.ImageModerationWithOptions(imageModerationRequest, runtime)
+	if err != nil {
+		return list, err
+	}
+	statusCode := tea.IntValue(tea.ToInt(response.StatusCode))
+	body := response.Body
+	imageModerationResponseData := body.Data
+	if statusCode == http.StatusOK {
+		if tea.IntValue(tea.ToInt(body.Code)) == 200 {
+			result := imageModerationResponseData.Result
+			list = make([]*ImageModerationInfo, 0)
+			for i := 0; i < len(result); i++ {
+				label := tea.StringValue(result[i].Label)
+				confidence := tea.Float32Value(result[i].Confidence)
+				if _, ok := req.Labels[label]; ok && confidence > req.Labels[label] {
+					list = append(list, &ImageModerationInfo{
+						Label:      label,
+						Confidence: confidence,
+					})
+				}
+			}
+			if len(list) > 0 {
+				return list, errors.New("invalid image")
+			}
+			return list, nil
+		}
+		return list, errors.New("image moderation not success. status" + fmt.Sprintf("%d", tea.IntValue(tea.ToInt(body.Code))))
+	}
+	return list, errors.New("image moderation failed. statusCode:" + tea.ToString(statusCode))
+}
--- aliyun/green_test.go
+++ aliyun/green_test.go
@@ -42,3 +42,45 @@ func TestGreenClient_ImageModeration(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestGreenClient_ImageModerationAdvance(t *testing.T) {
+	client, err := NewGreenClient(os.Getenv("AccessKeyId"), os.Getenv("AccessKeySecret"), os.Getenv("Endpoint"))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	list, err := client.ImageModerationAdvance(&ImageModerationRequest{
+		OssBucketName: tea.String(os.Getenv("Bucket")),
+		OssObjectName: tea.String(os.Getenv("OssObjectName")),
+		OssRegionID:   tea.String(os.Getenv("OssRegion")),
+		Labels: map[string]float32{
+			"pornographic_adultContent":              60,
+			"pornographic_adultContent_tii":          60,
+			"sexual_suggestiveContent":               60,
+			"sexual_partialNudity":                   60,
+			"political_historicalNihility":           60,
+			"political_historicalNihility_tii":       60,
+			"political_politicalFigure":              60,
+			"political_politicalFigure_name_tii":     60,
+			"political_politicalFigure_metaphor_tii": 60,
+			"political_prohibitedPerson":             60,
+			"political_prohibitedPerson_tii":         60,
+			"political_taintedCelebrity":             60,
+			"political_taintedCelebrity_tii":         60,
+			"political_flag":                         60,
+			"political_map":                          60,
+			"political_religion_tii":                 60,
+			"contraband_gamble":                      60,
+			"contraband_gamble_tii":                  60,
+		},
+	})
+	if err != nil {
+		if len(list) > 0 {
+			for _, info := range list {
+				t.Log(info.Label, info.Confidence)
+			}
+		} else {
+			t.Error(err)
+		}
+	}
+}
