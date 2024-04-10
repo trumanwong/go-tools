7e53ecf6e596b62d53bd7608b4d1e8bc58e95b16
trumanwong <1415829005@qq.com>
2024-04-10 15:59:07 +0800

    add basic auth

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -1,20 +1,10 @@
 package crawler
 
 import (
-	"github.com/trumanwong/cryptogo"
 	"io"
 	"net/http"
 	"time"
 )
-
-func GenerateCookie() string {
-	mUID := cryptogo.MD5ToUpper([]byte(time.Now().Format(time.DateTime)))
-	gUID := cryptogo.MD5ToUpper([]byte(mUID))
-	dOB := time.Now().Format("20060102150405")
-	sID := cryptogo.MD5ToUpper([]byte(dOB))
-	result := "MUID=" + mUID + "; SNRHOP=I=&TS=; SRCHD=AF=NOFORM; SRCHHPGUSR=SRCHLANG=zh-Hans; SRCHUID=V=2&GUID=" + gUID + "&dmnchg=1; SRCHUSR=DOB=" + dOB + "; SUID=M; _EDGE_S=F=1&SID=" + sID + "; _EDGE_V=1; _SS=SID=" + sID + "; MUIDB=" + mUID
-	return result
-}
 
 type Request struct {
 	// 请求地址
@@ -28,7 +18,13 @@ type Request struct {
 	// 请求代理
 	Transport http.RoundTripper
 	// 请求超时时间
-	Timeout time.Duration
+	Timeout   time.Duration
+	BasicAuth *BasicAuth
+}
+
+type BasicAuth struct {
+	Username string
+	Password string
 }
 
 // Send 发送请求
@@ -38,11 +34,12 @@ func Send(request *Request) (*http.Response, error) {
 		Timeout:   request.Timeout,
 	}
 	req, err := http.NewRequest(request.Method, request.Url, request.Body)
-
 	if err != nil {
 		return nil, err
 	}
-
+	if request.BasicAuth != nil {
+		req.SetBasicAuth(request.BasicAuth.Username, request.BasicAuth.Password)
+	}
 	if request.Headers != nil {
 		for k, v := range request.Headers {
 			req.Header.Set(k, v)
