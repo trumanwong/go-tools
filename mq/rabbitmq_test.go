9826a41c2c98da3b717dcf22cbe3d6127a9c00b1
trumanwong <1415829005@qq.com>
2024-05-20 09:26:31 +0800

    add example

--- mq/rabbitmq_test.go
+++ mq/rabbitmq_test.go
@@ -1,12 +1,68 @@
 package mq
 
 import (
+	"context"
 	"fmt"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"os"
 	"testing"
 	"time"
 )
+
+func Example() {
+	queue := NewRabbitMQ(&Options{
+		Name: os.Getenv("RABBITMQ_QUEUE_NAME"),
+		//  "amqp://guest:guest@localhost:5672/"
+		Addr:          os.Getenv("RABBITMQ_ADDR"),
+		PrefetchCount: 1,
+		PrefetchSize:  0,
+		Global:        false,
+		Consume: func(msgs <-chan amqp.Delivery) {
+			for d := range msgs {
+				fmt.Println("receive data: ", string(d.Body))
+				time.Sleep(10 * time.Second)
+				_ = d.Ack(false)
+			}
+		},
+	})
+	message := []byte("message")
+
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(time.Second*20))
+	defer cancel()
+loop:
+	for {
+		select {
+		// Attempt to push a message every 2 seconds
+		case <-time.After(time.Second * 2):
+			if err := queue.Push(message); err != nil {
+				fmt.Printf("Push failed: %s\n", err)
+			} else {
+				fmt.Println("Push succeeded!")
+			}
+		case <-ctx.Done():
+			queue.Close()
+			break loop
+		}
+	}
+}
+
+func Example_consume() {
+	NewRabbitMQ(&Options{
+		Name: os.Getenv("RABBITMQ_QUEUE_NAME"),
+		//  "amqp://guest:guest@localhost:5672/"
+		Addr:          os.Getenv("RABBITMQ_ADDR"),
+		PrefetchCount: 1,
+		PrefetchSize:  0,
+		Global:        false,
+		Consume: func(msgs <-chan amqp.Delivery) {
+			for d := range msgs {
+				fmt.Println("receive data: ", string(d.Body))
+				time.Sleep(10 * time.Second)
+				_ = d.Ack(false)
+			}
+		},
+	})
+}
 
 func TestNewRabbitMQ(t *testing.T) {
 	arguments := make(amqp.Table)
