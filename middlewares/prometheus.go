3929b7621f2a3d5064caac725238ce4db8b82892
trumanwong <1415829005@qq.com>
2024-10-23 16:20:12 +0800

    fix bug

--- middlewares/prometheus.go
+++ middlewares/prometheus.go
@@ -4,79 +4,101 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/trumanwong/go-tools/helper"
 	"net/http"
+	"time"
 )
 
 type prometheusMiddleware struct {
-	buckets  []float64
-	registry prometheus.Registerer
+	buckets         []float64
+	notStatisticUri []string
+	requestsTotal   *prometheus.CounterVec
+	requestDuration *prometheus.HistogramVec
+	requestSize     *prometheus.SummaryVec
+	responseSize    *prometheus.SummaryVec
 }
 
-func NewPrometheusMiddleware(buckets []float64, registry prometheus.Registerer) Middleware {
+func NewPrometheusMiddleware(buckets []float64, notStatisticUri []string) Middleware {
 	if buckets == nil {
 		buckets = prometheus.ExponentialBuckets(0.1, 1.5, 5)
 	}
+
+	requestsTotal := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Tracks the number of HTTP requests.",
+		}, []string{"method", "host", "uri", "handler"},
+	)
+	requestDuration := prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_duration_seconds",
+			Help:    "Tracks the latencies for HTTP requests.",
+			Buckets: buckets,
+		},
+		[]string{"method", "uri", "handler", "status"},
+	)
+	requestSize := prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name: "http_request_size_bytes",
+			Help: "Tracks the size of HTTP requests.",
+		},
+		[]string{"method", "uri", "handler"},
+	)
+	responseSize := prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name: "http_response_size_bytes",
+			Help: "Tracks the size of HTTP responses.",
+		},
+		[]string{"method", "uri", "handler", "status"},
+	)
+	prometheus.MustRegister(
+		requestsTotal,
+		requestDuration,
+		requestSize,
+		responseSize,
+	)
 	return &prometheusMiddleware{
-		buckets:  buckets,
-		registry: registry,
+		buckets:         buckets,
+		notStatisticUri: notStatisticUri,
+		requestsTotal:   requestsTotal,
+		requestDuration: requestDuration,
+		requestSize:     requestSize,
+		responseSize:    responseSize,
 	}
 }
 
 func (m *prometheusMiddleware) Handle() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		reg := prometheus.WrapRegistererWith(prometheus.Labels{"handler": ctx.HandlerName()}, m.registry)
-
-		requestsTotal := promauto.With(reg).NewCounterVec(
-			prometheus.CounterOpts{
-				Name: "http_requests_total",
-				Help: "Tracks the number of HTTP requests.",
-			}, []string{"method", "code"},
-		)
-		requestDuration := promauto.With(reg).NewHistogramVec(
-			prometheus.HistogramOpts{
-				Name:    "http_request_duration_seconds",
-				Help:    "Tracks the latencies for HTTP requests.",
-				Buckets: m.buckets,
-			},
-			[]string{"method", "code"},
-		)
-		requestSize := promauto.With(reg).NewSummaryVec(
-			prometheus.SummaryOpts{
-				Name: "http_request_size_bytes",
-				Help: "Tracks the size of HTTP requests.",
-			},
-			[]string{"method", "code"},
-		)
-		responseSize := promauto.With(reg).NewSummaryVec(
-			prometheus.SummaryOpts{
-				Name: "http_response_size_bytes",
-				Help: "Tracks the size of HTTP responses.",
-			},
-			[]string{"method", "code"},
-		)
+		start := time.Now()
 		ctx.Next()
+		if m.notStatisticUri != nil && helper.InArray(ctx.Request.RequestURI, m.notStatisticUri) {
+			return
+		}
 
 		status := fmt.Sprintf("%d", ctx.Writer.Status())
-		requestsTotal.WithLabelValues(
+		m.requestsTotal.WithLabelValues(
 			ctx.Request.Method,
+			ctx.Request.Host,
 			ctx.Request.RequestURI,
-			status,
+			ctx.HandlerName(),
 		).Inc()
-		requestDuration.WithLabelValues(
+		m.requestDuration.WithLabelValues(
 			ctx.Request.Method,
 			ctx.Request.RequestURI,
+			ctx.HandlerName(),
 			status,
-		)
-		requestSize.WithLabelValues(
+		).Observe(time.Since(start).Seconds())
+		m.requestSize.WithLabelValues(
 			ctx.Request.Method,
 			ctx.Request.RequestURI,
-		)
-		responseSize.WithLabelValues(
+			ctx.HandlerName(),
+		).Observe(float64(computeApproximateRequestSize(ctx.Request)))
+		m.responseSize.WithLabelValues(
 			ctx.Request.Method,
 			ctx.Request.RequestURI,
+			ctx.HandlerName(),
 			status,
-		)
+		).Observe(float64(ctx.Writer.Size()))
 	}
 }
 
