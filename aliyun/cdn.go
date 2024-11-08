b8e3652a2806d983b0b75fc400bbe2cc67f3a640
trumanwong <1415829005@qq.com>
2024-11-08 09:33:48 +0800

    add AnalyzeCdnAccessLog

--- aliyun/cdn.go
+++ aliyun/cdn.go
@@ -1,9 +1,16 @@
 package aliyun
 
 import (
+	"bufio"
+	"compress/gzip"
+	"fmt"
 	cdn20180510 "github.com/alibabacloud-go/cdn-20180510/v4/client"
 	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
 	util "github.com/alibabacloud-go/tea-utils/v2/service"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 type CdnClient struct {
@@ -43,4 +50,121 @@ func (c CdnClient) RefreshObjectCachesWithOptions(req *cdn20180510.RefreshObject
 
 func (c CdnClient) DescribeDomainUsageData(req *cdn20180510.DescribeDomainUsageDataRequest, runtime *util.RuntimeOptions) (*cdn20180510.DescribeDomainUsageDataResponse, error) {
 	return c.client.DescribeDomainUsageDataWithOptions(req, runtime)
+}
+
+func (c CdnClient) DescribeCdnDomainLogs(req *cdn20180510.DescribeCdnDomainLogsRequest, runtime *util.RuntimeOptions) (*cdn20180510.DescribeCdnDomainLogsResponse, error) {
+	return c.client.DescribeCdnDomainLogsWithOptions(req, runtime)
+}
+
+// CdnAccessLog https://help.aliyun.com/zh/cdn/user-guide/download-logs?spm=a2c4g.11186623.help-menu-27099.d_2_7_0_1.40dd77db3L8Tlf
+type CdnAccessLog struct {
+	// 请求结束时间
+	ReqTime string
+	// 从用户请求携带的请求头X-Forwarded-For中提取左边第一个IP地址（即client_ip，客户端IP，如果客户端与CDN节点之间没有经过代理的话，等同于客户端与CDN节点建联使用的IP）。
+	ClientIp string
+	// 从用户请求携带的请求头X-Forwarded-For中提取左边第二个IP地址（即proxy_ip，代理IP，如果客户端与CDN节点之间没有经过代理的话，提取到的空值用-表示）。
+	ProxyIp string
+	// 请求响应时间，单位为毫秒
+	RespTime float64
+	// HTTP请求头中的Referer。
+	Referer string
+	// 请求方法
+	Method string
+	// 用户请求的URL链接。
+	Url string
+	// HTTP状态码。
+	Status int64
+	// 请求大小，单位为字节
+	ReqSize int64
+	// CDN 向用户传输的数据量，单位是 bytes。
+	RespSize int64
+	// 命中信息
+	StatusHit string
+	// 请求中 User-Agent 头部的值。如果请求不包含该头部，该字段的值是 -
+	UserAgent string
+	// CDN 响应中 Content-Type 头部的值。如果响应不包含该头部，该字段的值是 -。
+	ContentType string
+	// 建连IP地址
+	IP string
+}
+
+func (c CdnClient) AnalyzeCdnAccessLog(logPath string, handler func(*CdnAccessLog) error) error {
+	fileInfoList, err := os.ReadDir(logPath)
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < len(fileInfoList); i++ {
+		err = func(i int) error {
+			file, err := os.Open(logPath + "/" + fileInfoList[i].Name())
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+
+			gz, err := gzip.NewReader(file)
+			if err != nil {
+				return err
+			}
+			defer gz.Close()
+			scanner := bufio.NewScanner(gz)
+			for scanner.Scan() {
+				text := scanner.Text()
+				strs := strings.Split(text, "\n")
+				for _, str := range strs {
+					pattern := `\[(.*?)\] (\S+) (\S+) (\S+) "(.*?)" "(\S+) (.*?)" (\d+) (\d+) (\d+) (\S+) "(.*?)" "(.*?)" (\S+)`
+					re := regexp.MustCompile(pattern)
+					// Find the matches
+					info := re.FindStringSubmatch(str)
+					if len(info) != 15 {
+						return fmt.Errorf("invalid log format, expect 15 fields, get %d fields: %s", len(info), str)
+					}
+
+					respTime, err := strconv.ParseFloat(info[4], 64)
+					if err != nil {
+						return fmt.Errorf("invalid respTime: %s, expect float: %s", info[4], str)
+					}
+
+					statusCode, err := strconv.ParseInt(info[8], 10, 64)
+					if err != nil {
+						return fmt.Errorf("invalid statusCode: %s, expect int: %s", info[8], str)
+					}
+					reqSize, err := strconv.ParseInt(info[9], 10, 64)
+					if err != nil {
+						return fmt.Errorf("invalid reqSize: %s, expect int: %s", info[9], str)
+					}
+					respSize, err := strconv.ParseInt(info[10], 10, 64)
+					if err != nil {
+						return fmt.Errorf("invalid respSize: %s, expect int: %s", info[10], str)
+					}
+					accessLog := CdnAccessLog{
+						ReqTime:     info[1],
+						ClientIp:    info[2],
+						ProxyIp:     info[3],
+						RespTime:    respTime,
+						Referer:     info[5],
+						Method:      info[6],
+						Url:         info[7],
+						Status:      statusCode,
+						ReqSize:     reqSize,
+						RespSize:    respSize,
+						StatusHit:   info[11],
+						UserAgent:   info[12],
+						ContentType: info[13],
+						IP:          info[14],
+					}
+					err = handler(&accessLog)
+					if err != nil {
+						return err
+					}
+				}
+			}
+			return nil
+		}(i)
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- aliyun/cdn_test.go
+++ aliyun/cdn_test.go
@@ -1,13 +1,17 @@
 package aliyun
 
 import (
+	"fmt"
 	cdn20180510 "github.com/alibabacloud-go/cdn-20180510/v4/client"
 	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
 	util "github.com/alibabacloud-go/tea-utils/v2/service"
 	"github.com/alibabacloud-go/tea/tea"
+	"github.com/trumanwong/go-tools/crawler"
 	"github.com/trumanwong/go-tools/helper"
 	"log"
+	"net/url"
 	"os"
+	"path/filepath"
 	"strconv"
 	"testing"
 	"time"
@@ -109,4 +113,66 @@ func TestCdnClient_DescribeDomainUsageData(t *testing.T) {
 		total += flux
 	}
 	log.Println(helper.FormatByte(float64(total), 1024))
+}
+
+func TestCdnClient_DescribeCdnDomainLogs(t *testing.T) {
+	client, err := NewCdnClient(&openapi.Config{
+		AccessKeyId:     tea.String(os.Getenv("AccessKeyId")),
+		AccessKeySecret: tea.String(os.Getenv("AccessKeySecret")),
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	intervalTime := helper.GetIntervalTime(&helper.GetIntervalTimeRequest{
+		Time: time.Now(),
+		Type: helper.Day,
+		Num:  -1,
+	})
+	log.Println(intervalTime.StartAt.UTC().Format("2006-01-02T15:04:05Z"))
+	log.Println(intervalTime.EndAt.UTC().Format("2006-01-02T15:04:05Z"))
+	resp, err := client.DescribeCdnDomainLogs(&cdn20180510.DescribeCdnDomainLogsRequest{
+		DomainName: tea.String(os.Getenv("DomainName")),
+		StartTime:  tea.String(intervalTime.StartAt.UTC().Format("2006-01-02T15:04:05Z")),
+		EndTime:    tea.String(intervalTime.EndAt.UTC().Format("2006-01-02T15:04:05Z")),
+	}, &util.RuntimeOptions{})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	for _, v := range resp.Body.DomainLogDetails.DomainLogDetail {
+		log.Println(*v.LogCount)
+		for _, detail := range v.LogInfos.LogInfoDetail {
+			log.Println(*detail.LogSize, *detail.LogPath)
+			link := fmt.Sprintf("https://%s", *detail.LogPath)
+			u, _ := url.Parse(link)
+			fileName := filepath.Base(u.Path)
+			_, err = helper.DownloadFile(&crawler.Request{
+				Url: link,
+			}, fmt.Sprintf("temp/%s/%s", time.Now().Format(time.DateOnly), fileName), true)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+		}
+	}
+}
+
+func TestCdnClient_AnalyzeCdnAccessLog(t *testing.T) {
+	client, err := NewCdnClient(&openapi.Config{
+		AccessKeyId:     tea.String(os.Getenv("AccessKeyId")),
+		AccessKeySecret: tea.String(os.Getenv("AccessKeySecret")),
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = client.AnalyzeCdnAccessLog(os.Getenv("LogPath"), func(accessLog *CdnAccessLog) error {
+		return nil
+	})
+	if err != nil {
+		t.Error(err)
+	}
 }
