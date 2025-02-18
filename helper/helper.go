15c7eb624029d080d8100e0a7a0a4687561f00be
trumanwong <1415829005@qq.com>
2025-02-18 17:58:41 +0800

    add IsASCII

--- helper/helper.go
+++ helper/helper.go
@@ -588,3 +588,13 @@ func ReadUTF16File(filename string) (string, error) {
 	runes := utf16.Decode(u16s)
 	return string(runes), nil
 }
+
+// IsASCII checks if a given string contains only ASCII characters.
+func IsASCII(s string) bool {
+	for i := 0; i < len(s); i++ {
+		if s[i] > 127 {
+			return false
+		}
+	}
+	return true
+}
